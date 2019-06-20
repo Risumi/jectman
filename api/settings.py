a4dda2ee4a1bbb90954b6cdf3698b1045a335b81
Risumi <rizkysuhaimi@gmail.com>
2019-06-21 01:21:38 +0700

    minor fix

--- api/settings.py
+++ api/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.WMessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
